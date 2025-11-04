f84fdcafefc0d54a5ec6dd58980ff3e3993e519d
Joshua Yamen <jdyamen@gmail.com>
2025-11-04 13:25:08 +1100

    Cleaned ui and added more ui elements, added play/pause functionality, added fileNameExtensionFilter to only select .wav, .aiff, and .au files

--- AudioPlayer.java
+++ AudioPlayer.java
@@ -2,38 +2,76 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.io.*;
 import javax.sound.sampled.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 class AudioPlayer {
+    // UI
+    int RESOLUTION_X = 300;
+    int RESOLUTION_Y = 400;
+    JFrame frame;
+    JFileChooser fileChooser;
+    JButton playButton, openButton, forwardButton, backwardButton;
+    JSlider progresSlider;
+    JLabel currentTime, endTime;
+
+    // Audio
+    boolean playing = false;
     File file;
     Clip clip;
     AudioInputStream audioInputStream;
+    FileNameExtensionFilter filter = new FileNameExtensionFilter("Audio Files (*.wav, *.aiff, *.au)", 
+    "wav", ".aiff", ".au");
 
-    // UI
-    JFrame frame;
-    JFileChooser fileChooser;
-    JButton playButton, openButton;
 
     public AudioPlayer() {
         frame = new JFrame();
-        frame.setSize(800, 600);
+        frame.setSize(RESOLUTION_X, RESOLUTION_Y);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
         fileChooser = new JFileChooser();
+        fileChooser.setFileFilter(filter);
+
+        progresSlider = new JSlider();
+        progresSlider.setBounds(10, 250, RESOLUTION_X - 10, 20);
+        frame.add(progresSlider);
+
+        currentTime = new JLabel("--:--");
+        endTime = new JLabel("--:--");
+        currentTime.setBounds(30, 300, 80, 25);
+        endTime.setBounds(230, 300, 80, 25);
+        frame.add(currentTime);
+        frame.add(endTime);
 
         playButton = new JButton("Play");
-        playButton.setBounds(350, 500, 100, 25);
+        playButton.setBounds(115, 325, 70, 25);
         frame.add(playButton);
         playButton.setEnabled(false);
 
         playButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                clip.start();
+                if(!playing) {
+                    clip.start();
+                    playing = true;
+                    playButton.setText("Pause");
+                } else {
+                    clip.stop();
+                    playing = false;
+                    playButton.setText("Play");
+                }
             }
         });
 
-        openButton = new JButton("Open file");
-        openButton.setBounds(20, 20, 100, 100);
+        backwardButton = new JButton("<<");
+        backwardButton.setBounds(65, 325, 50, 25);
+        frame.add(backwardButton);
+
+        forwardButton = new JButton(">>");
+        forwardButton.setBounds(185, 325, 50, 25);
+        frame.add(forwardButton);
+
+        openButton = new JButton("Open");
+        openButton.setBounds(10, 10, 70, 25);
         frame.add(openButton);
 
         openButton.addActionListener(new ActionListener() {
@@ -41,15 +79,23 @@ class AudioPlayer {
             public void actionPerformed(ActionEvent e) {
                 fileChooser.showOpenDialog(frame);
                 file = fileChooser.getSelectedFile();
+                long seconds;
+                long minutes;
+
                 try {
-                    audioInputStream = AudioSystem.getAudioInputStream(file);
-                    clip = AudioSystem.getClip();
-                    clip.open(audioInputStream);
+                    if(file != null) {
+                        audioInputStream = AudioSystem.getAudioInputStream(file);
+                        clip = AudioSystem.getClip();
+                        clip.open(audioInputStream);
+                        playButton.setEnabled(true);
+                        seconds = (clip.getMicrosecondLength() / 1000000) % 60;
+                        minutes = (clip.getMicrosecondLength() / 1000000) / 60;
+                        endTime.setText(String.valueOf(minutes) + ":" + String.valueOf(seconds));
+                    }
                 } catch (Exception ex) {
                     ex.printStackTrace();
                     JOptionPane.showMessageDialog(frame, "Error: " + ex.getMessage());
                 }
-                playButton.setEnabled(true);
             }
         });
 
